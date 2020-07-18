8bf391f640716ca5f59516aafc19865e6e80a060
forest93 <forest93@163.com>
2020-07-18 22:30:10 +0800

    Deps upgrade. Get CodeMirror.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
-import transpilerCore from "hedgehog-lab/transpiler_core";
-import { executeOutput } from "hedgehog-lab/hedgehog_runtime";
+import transpilerCore from "hedgehog-lab/core/transpiler/transpiler-core";
+import { executeOutput } from "hedgehog-lab/core/runtime";
+import { EditorState, EditorView, basicSetup } from "@codemirror/next/basic-setup";
+import { javascript } from "@codemirror/next/lang-javascript";
 
 export const App: React.FC = () => {
     function executeSomething() {
@@ -9,9 +11,21 @@ print(x + x)`);
         const result = executeOutput(code);
         window.alert(JSON.stringify(result));
     }
+    const editorState = React.useMemo(() => EditorState.create({ doc: `print("Hello, world!");`, extensions: [basicSetup, javascript()] }), []);
+    const editorViewRef = React.useRef<EditorView>();
+    function onEditorContainerChanged(domContainer: HTMLElement | null): void {
+        if (editorViewRef.current) {
+            editorViewRef.current.destroy();
+            editorViewRef.current = undefined;
+        }
+        if (domContainer) {
+            editorViewRef.current = new EditorView({ state: editorState, parent: domContainer });
+        }
+    }
     return (
         <div>
             <h2>It works.</h2>
+            <div ref={onEditorContainerChanged} />
             <button type="button" onClick={executeSomething}>Execute something</button>
         </div>
     );
--- src/hedgehog-lab.d.ts
+++ src/hedgehog-lab.d.ts
@@ -1,11 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-declare module "hedgehog-lab/transpiler_core" {
+declare module "hedgehog-lab/core/transpiler/transpiler-core" {
     function transpilerCore(yourCode: string): string;
     export default transpilerCore;
 }
 
-declare module "hedgehog-lab/hedgehog_runtime" {
+declare module "hedgehog-lab/core/runtime" {
     export interface IExecutionOutput {
         outputType: "print",
         text: {
